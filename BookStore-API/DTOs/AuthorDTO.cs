cbdef8bdc920018b1f30efb22196d59237d946ad
Trevoir Williams <trevoir.williams@gmail.com>
2020-07-24 10:20:57 -0500

    Pre WASM Version

--- BookStore-API/DTOs/AuthorDTO.cs
+++ BookStore-API/DTOs/AuthorDTO.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace BookStore_API.DTOs
 {
     public class AuthorDTO
-    {
+    { 
         public int Id { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
